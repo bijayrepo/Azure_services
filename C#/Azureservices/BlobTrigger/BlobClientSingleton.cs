ad4595a748ae7e50c3c7cec561a38b1a24b81b97
BIJAY KUMAR MURMU <edubijaykumar2000@gmail.com>
2025-11-09 20:56:14 +0530

    Add Blogtrigger function for image upload

---
+++ C#/Azureservices/BlobTrigger/BlobClientSingleton.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azureservices.BlobTrigger
+{
+    internal sealed class BlobClientSingleton
+    {
+        private static readonly Lazy<BlobClientSingleton> instance =
+        new Lazy<BlobClientSingleton>(() => new BlobClientSingleton());
+
+        private readonly BlobServiceClient blobServiceClient;
+
+        private BlobClientSingleton()
+        {
+            string blobConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            blobServiceClient = new BlobServiceClient(blobConnectionString);
+        }
+
+        public static BlobClientSingleton Instance => instance.Value;
+
+        public BlobServiceClient GetBlobServiceClient()
+        {
+            return blobServiceClient;
+        }
+
+    }
+}
--- C#/Azureservices/BlobTrigger/FileUploadManager.cs
+++ C#/Azureservices/BlobTrigger/FileUploadManager.cs
@@ -7,9 +7,19 @@ namespace Azureservices.BlobTrigger
     public static class FileUploadManager
     {
         [FunctionName("FileUploadManager")]
-        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "")]Stream myBlob, string name, TraceWriter log)
+        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            // Get BlobClient
+            var blobServiceClient = BlobClientSingleton.Instance.GetBlobServiceClient();
+            var containerClient = blobServiceClient.GetBlobContainerClient("images");
+            var blobClient = containerClient.GetBlobClient(name);
+
+            // Construct public URL
+            string publicUrl = blobClient.Uri.ToString();
+            log.Info($"Public URL: {publicUrl}");
+
         }
     }
 }
